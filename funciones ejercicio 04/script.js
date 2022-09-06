501fef20bc5e65bebd05b54777e72abc63faba4b
Hectorwearecode <wearecode04@gmail.com>
2022-09-06 06:40:17 +0200

    ejercicio 4 tema 10

--- funciones ejercicio 04/script.js
+++ funciones ejercicio 04/script.js
@@ -1,19 +1,22 @@
-var numerouno = parseInt(prompt("Escribe un número"));
-var numerodos = parseInt(prompt("Escribe otro número"));
-alert(numerouno/numerodos);
-function divisionNumeros(numerouno,numerodos) {
-      alert(numerouno/numerodos);
+var numerouno = parseInt(prompt("Escribe la cantidad en euros"));
+var moneda = prompt("Escribe la moneda a la que quieres pasar los euros")
+function cambioMoneda(numerouno,moneda) {
+     switch(moneda){
+        case "libras":
+            alert(numerouno*0.86);
+        case "yenes":
+            alert(numerouno*129.852);
+        case "dólares":
+            alert(numerouno*1.28611);
+     } 
     }
-  while (numerodos != 0){
-    numerouno = parseInt(prompt("Escribe un número"));
-    numerodos = parseInt(prompt("Escribe otro número"));
-    divisionNumeros(numerouno,numerodos);
-  }
-  if(numerodos == 0){
-    console.log("Ha terminado");
-  }
-  /*Escribe un programa que pida al usuario dos números. 
-      Usa esos 2 números en una función que divide el primero por el segundo
-      y devuelve el resultado. Muestra el resultado que devuelve la función 
-      en un alert. Después vuelve a pedir los números y hacer todo otra vez 
-      hasta que el usuario introduzca un 0 como segundo número.*/
+    cambioMoneda(numerouno,moneda);
+  /* Crea un aplicación que nos convierta una cantidad de euros 
+      introducida por teclado a otra moneda, estas pueden ser a dólares, yenes o libras. 
+      El método tendrá como parámetros, la cantidad de euros 
+      y la moneda a pasar que será un string. Este no devolverá ningún valor, 
+      mostrará un mensaje indicando el cambio.
+      
+            a. 0.86 libras es un 1 €
+            b. 1.28611 $ es un 1 €
+            c. 129.852 yenes es un 1 € -*/
