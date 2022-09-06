e3486f6adf24e3e00c2cf6373cff5229cf15ac8e
Hectorwearecode <wearecode04@gmail.com>
2022-09-06 06:00:17 +0200

    ejercicio 3 tema 10

---
+++ ejercicio 03/script.js
@@ -0,0 +1,18 @@
+var numerouno = parseInt(prompt("Escribe un número"));
+var numerodos = parseInt(prompt("Escribe otro número"));
+function divisionNumeros(numerouno,numerodos) {
+      alert(numerouno/numerodos);
+    }
+  while (numerodos != 0){
+    numerouno = parseInt(prompt("Escribe un número"));
+    numerodos = parseInt(prompt("Escribe otro número"));
+    divisionNumeros(numerouno,numerodos);
+  }
+  if(numerodos == 0){
+    console.log("Ha terminado");
+  }
+  /*Escribe un programa que pida al usuario dos números. 
+      Usa esos 2 números en una función que divide el primero por el segundo
+      y devuelve el resultado. Muestra el resultado que devuelve la función 
+      en un alert. Después vuelve a pedir los números y hacer todo otra vez 
+      hasta que el usuario introduzca un 0 como segundo número.*/
