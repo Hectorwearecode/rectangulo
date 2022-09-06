5a8d660f7fcf9c415fcc80e6208936928be84dd3
Hectorwearecode <wearecode04@gmail.com>
2022-09-06 04:58:52 +0200

    ejercicio 1 funciones

---
+++ funciones_ejercicio_01/script.js
@@ -0,0 +1,11 @@
+function muestraHola(numeroVeces) {
+    for(var i = 0; i < numeroVeces; i++){
+      console.log("Hola");
+    }
+  }
+  
+  // Llamada a la función
+  muestraHola(5);
+  muestraHola(3);
+  
+  
